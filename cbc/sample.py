b29ed263c065aef03e2feda95fc9a1e1df5e702c
exempll <alfonsotabo@gmail.com>
2020-09-21 13:01:41 +0200

    (unsuccesfull) decryption of m2

--- cbc/sample.py
+++ cbc/sample.py
@@ -1,5 +1,7 @@
 from oracle import *
+from copy import copy
 import sys
+import numpy as np
 
 if len(sys.argv) < 2:
     print "Usage: python sample.py <filename>"
@@ -11,7 +13,67 @@ f.close()
 
 ctext = [(int(data[i:i+2],16)) for i in range(0, len(data), 2)]
 
+IV = ctext[:0x10]
+c = [ctext[x:0x10+x] for x in range(0x10,len(ctext), 0x10)]
+
+m = [[-1] * 0x10 for _ in range(2)]
+
+# Nothing to do with the padding, just the one-time pad
+pads = [[-1] * 0x10 for _ in range(2)]
+
+IV = np.array(IV)
+c = np.array(c)
+m = np.array(m)
+pads = np.array(pads)
+
 Oracle_Connect()
+
+padding = 0
+for i in range(0x10):
+    tatt = copy(ctext)
+    tatt[0x10+i] += 1
+    tatt[0x10+i] %= 0x100
+    rc = Oracle_Send(tatt, 3)
+    if not rc:
+        padding = 0x10-i
+        break
+print "Padding: %d" % padding
+
+m[-1, -padding:] = [padding]*padding
+pads[-1, -padding:] = m[-1, -padding:] ^ c[-1, -padding:]
+
+def testc(catt):
+    tatt = catt.flatten()
+    return Oracle_Send(list(IV) + list(tatt), 3)
+
+for i in range(padding, 0x10):
+    # Create message with padding that is 1 byte longer
+    matt = np.copy(m)
+    matt[-1, -i-1] = padding
+    matt[-1, -i-1:] += 1
+    
+    # Calculate the cipher for the part of the message
+    # we know the pads for
+    catt = np.copy(c)
+    catt[-1, -i:] = matt[-1, -i:] ^ pads[-1, -i:]
+
+    # Try out different pads for the newly padded byte
+    for p in range(0x100):
+        catt[-1, -i-1] = matt[-1, -i-1] ^ p
+        rc = testc(catt)
+        if rc:
+            print(i, p)
+    # Store the correct pad
+    pads[-1, -i-1] = p
+
+    import pdb; pdb.set_trace()
+
+m[-1] = c[-1] ^ pads[-1]
+
+def decode(a):
+    return ''.join(chr(x) for x in a)
+
+print "Last block decrypted: %s" % decode(m[-1, :-padding]) 
 
 rc = Oracle_Send(ctext, 3)
 print "Oracle returned: %d" % rc
